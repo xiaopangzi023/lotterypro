3d34460165c59bf1c70b1c483ac6b39db90f1eda
xiaopangzi023 <761966652@qq.com>
2026-06-16 15:06:05 +0800

    Add service worker for caching assets

---
+++ sw.js
@@ -0,0 +1,24 @@
+const CACHE_NAME = "lotterypro-v1";
+
+const urlsToCache = [
+  "./",
+  "./index.html",
+  "./app.js",
+  "./manifest.json"
+];
+
+self.addEventListener("install", event => {
+  event.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(urlsToCache))
+  );
+});
+
+self.addEventListener("fetch", event => {
+  event.respondWith(
+    caches.match(event.request)
+      .then(response => {
+        return response || fetch(event.request);
+      })
+  );
+});
